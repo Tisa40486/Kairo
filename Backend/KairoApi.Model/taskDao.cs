c67ff8e0aa1bc270425bbc651e37681e32a8dec3
Tisa_48 <130873934+Tisa40486@users.noreply.github.com>
2026-04-26 00:52:54 +0200

    add taskdao

---
+++ Backend/KairoApi.Model/taskDao.cs
@@ -0,0 +1,16 @@
+﻿using KairoApi.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace KairoApi.Model
+{
+    [Table("KairoApi_Task")]
+    public class TaskDao : IModelDao
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; } 
+    }
+}
