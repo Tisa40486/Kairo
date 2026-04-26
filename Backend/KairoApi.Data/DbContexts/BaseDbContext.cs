11fdba54e36ad661212b5b331153b1353894f32b
Tisa_48 <130873934+Tisa40486@users.noreply.github.com>
2026-04-26 02:20:19 +0200

    add baseContexts

---
+++ Backend/KairoApi.Data/DbContexts/BaseDbContext.cs
@@ -0,0 +1,17 @@
+﻿using KairoApi.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace KairoApi.Data.DbContexts
+{
+    public class BaseDbContext : DbContext, IBaseDbContext
+    {
+        public BaseDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
+        public new EntityEntry<TModelDao> Entry<TModelDao>(TModelDao entry) where TModelDao : class, IModelDao
+        {
+            return base.Entry<TModelDao>(entry);
+        }
+    }
+}
---
+++ Backend/KairoApi.Data/DbContexts/IBaseDbContext.cs
@@ -0,0 +1,13 @@
+﻿using KairoApi.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace KairoApi.Data.DbContexts
+{
+    public interface IBaseDbContext
+    {
+        EntityEntry<TModelDao> Entry<TModelDao>(TModelDao entry) where TModelDao : class, IModelDao;
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess = true, CancellationToken cancellationToken = new CancellationToken());
+    }
+}
