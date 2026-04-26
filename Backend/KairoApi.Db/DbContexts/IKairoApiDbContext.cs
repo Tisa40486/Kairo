103860761fa5aa4ba36183d7aded39149f4fbb52
Tisa_48 <130873934+Tisa40486@users.noreply.github.com>
2026-04-26 02:19:54 +0200

    add dbcontexts

---
+++ Backend/KairoApi.Db/DbContexts/IKairoApiDbContext.cs
@@ -0,0 +1,12 @@
+﻿using KairoApi.Data.DbContexts;
+using KairoApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace KairoApi.Db.DbContexts
+{
+    public interface IKairoApiDbContext : IBaseDbContext
+    {
+        public DbSet<UserDao> Users { get; set; }
+        public DbSet<TaskDao> Tasks { get; set; }
+    }
+}
---
+++ Backend/KairoApi.Db/DbContexts/KairoApiDbContext.cs
@@ -0,0 +1,21 @@
+﻿using KairoApi.Data.DbContexts;
+using KairoApi.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KairoApi.Db.DbContexts
+{
+    public class KairoApiDbContext : BaseDbContext, IKairoApiDbContext
+    {
+        public KairoApiDbContext(DbContextOptions<KairoApiDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserDao> Users { get; set; }
+        public DbSet<TaskDao> Tasks { get ; set;}
+
+    }
+}
