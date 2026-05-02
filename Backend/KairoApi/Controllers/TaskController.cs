96af276a1bc95b5b41e79017cabeff252efe07d7
Tisa_48 <130873934+Tisa40486@users.noreply.github.com>
2026-05-03 01:55:25 +0200

    task controller

---
+++ Backend/KairoApi/Controllers/TaskController.cs
@@ -0,0 +1,27 @@
+﻿using KairoApi.Business.Task.Query;
+using KairoApi.Dto;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KairoApi.App.Controllers
+{
+    [ApiController]
+    [Route("api/task")]
+    public class TaskController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TaskController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("get/{id}")]
+        public async Task<ActionResult<TaskReponse?>> GetTaskByIdAsync(int id)
+        {
+            var result = await _mediator.Send(new GetTaskByIdQuery { Id = id });
+
+            return Ok(result);
+        }
+    }
+}
