0e5a444940e56eb9433cbb51fc95fe883dc5cf59
Tisa_48 <130873934+Tisa40486@users.noreply.github.com>
2026-05-03 01:54:34 +0200

    create TaskRepository

---
+++ Backend/KairoApi.Db/Repository/Implementation/TaskRepository.cs
@@ -0,0 +1,14 @@
+﻿using KairoApi.Data.Repository;
+using KairoApi.Model;
+using KairoApi.Db.DbContexts;
+
+
+namespace KairoApi.Db.Repository.Implementation
+{
+    public class TaskRepository : BaseRepository<IKairoApiDbContext, TaskDao>, ITaskRepository
+    {
+        public TaskRepository(IKairoApiDbContext context) : base(context)
+        {
+        }
+    }
+}
