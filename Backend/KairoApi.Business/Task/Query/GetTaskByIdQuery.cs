ad8d69d6288ca541126fd5fb81501c77a76fd7e9
Tisa_48 <130873934+Tisa40486@users.noreply.github.com>
2026-05-03 01:54:55 +0200

    Create GetTaskByIdQuery

---
+++ Backend/KairoApi.Business/Task/Query/GetTaskByIdQuery.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using KairoApi.Db.UnitOfWork;
+using KairoApi.Dto;
+using MediatR;
+
+namespace KairoApi.Business.Task.Query
+{
+    public class GetTaskByIdQuery : IRequest<TaskReponse>
+    {
+        public int Id { get; set; }
+
+    }
+    public class GetTaskByIdQueryHandler : IRequestHandler<GetTaskByIdQuery, TaskReponse?>
+    {
+        readonly IKairoApiUnitOfWork _uow;
+        readonly IMapper _mapper;
+
+        public GetTaskByIdQueryHandler(
+            IKairoApiUnitOfWork uow,
+            IMapper mapper)
+        {
+            _uow = uow;
+            _mapper = mapper;
+        }
+
+        public async Task<TaskReponse?> Handle(GetTaskByIdQuery request, CancellationToken cancellationToken)
+        {
+            var data = await _uow.TaskRepository.GetByIdAsync(request.Id);
+
+            if (data == null)
+                return null;
+
+            var result = _mapper.Map<TaskReponse>(data);
+            return result;
+        }
+
+    }
+}
