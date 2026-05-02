31aacff06fc919db30e2299f00b243125afc613a
Tisa_48 <130873934+Tisa40486@users.noreply.github.com>
2026-05-03 01:54:03 +0200

    Create BaseRepository

---
+++ Backend/KairoApi.Data/Repository/IBaseRepository.cs
@@ -0,0 +1,20 @@
+﻿using KairoApi.Data.DbContexts;
+using KairoApi.Data.Model;
+
+namespace KairoApi.Data.Repository
+{
+    public interface IBaseRepository<TContext, TModelDao>
+        where TModelDao : class, IModelDao
+        where TContext : IBaseDbContext
+    {
+        TContext _context { get; }
+        Task<IEnumerable<TModelDao>> GetAllAsync(bool withNoTracking = true);
+        Task<TModelDao?> GetByIdAsync(int id, bool withNoTracking = true);
+
+        Task AddAndSaveAsync(TModelDao entity);
+        Task UpdateAsync(TModelDao entity);
+        Task RemoveAsync(TModelDao entity);
+        Task RemoveByIdAsync(int id, bool withNoTracking = true);
+
+    }
+}
