dcd7503fa7e88164edcbcc7967ede945938d81aa
Tisa_48 <130873934+Tisa40486@users.noreply.github.com>
2026-05-03 01:55:15 +0200

    add profile

---
+++ Backend/KairoApi.Business/KairoApiProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using KairoApi.Dto;
+using KairoApi.Model;
+
+namespace KairoApi.Business
+{
+    public class KairoApiProfile : Profile
+    {
+        public KairoApiProfile() 
+        {
+            CreateMap<TaskDao, TaskReponse>();
+        }
+    }
+}
