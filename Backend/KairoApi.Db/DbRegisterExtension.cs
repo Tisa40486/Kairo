786d17dedfc27b794d2b564d58009361f721e34e
Tisa_48 <130873934+Tisa40486@users.noreply.github.com>
2026-05-03 01:53:27 +0200

    create dbRegisterExtension

---
+++ Backend/KairoApi.Db/DbRegisterExtension.cs
@@ -0,0 +1,20 @@
+﻿using KairoApi.Db.DbContexts;
+using KairoApi.Db.Repository;
+using KairoApi.Db.Repository.Implementation;
+using KairoApi.Db.UnitOfWork;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KairoApi.Db
+{
+    public static class DbRegisterExtension
+    {
+        public static void RegisterKairoApiDbContainer(this IServiceCollection services)
+        {
+            services.AddScoped<IKairoApiDbContext, KairoApiDbContext>();
+            services.AddScoped<ITaskRepository, TaskRepository>();
+
+
+            services.AddScoped<IKairoApiUnitOfWork, KairoApiUnitOfWork>();
+        }
+    }
+}
