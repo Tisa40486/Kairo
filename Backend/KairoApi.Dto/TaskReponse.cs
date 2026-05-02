9ac4e6649cedc47c2c5603ad7c73e100e431e050
Tisa_48 <130873934+Tisa40486@users.noreply.github.com>
2026-05-03 01:51:13 +0200

    Create TaskResponse

---
+++ Backend/KairoApi.Dto/TaskReponse.cs
@@ -0,0 +1,9 @@
+﻿namespace KairoApi.Dto
+{
+    public class TaskReponse
+    {
+        public int id { get; set; }
+        public required string Title { get; set; }
+        public string? Description { get; set; }
+    }
+}
