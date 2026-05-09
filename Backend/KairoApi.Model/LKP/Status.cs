d97f42a292eb7495fe2e0766d51a12a798644b34
Mattis Lefranc-Adam <lefrancadammattis@gmail.com>
2026-05-09 15:51:11 +0200

    create lkp_status

---
+++ Backend/KairoApi.Model/LKP/Status.cs
@@ -0,0 +1,10 @@
+﻿using KairoApi.Data.Model;
+
+namespace KairoApi.Model.LKP
+{
+    public class Status : IModelDao
+    {
+        public int id { get; set; }
+        public int Name { get; set; }
+    }
+}
